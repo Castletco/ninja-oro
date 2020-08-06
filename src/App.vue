21a8daf0af7212a71f5a4369791d0acd6309b647
DiegoCastillo <diegocastillonaranjo@gmail.com>
2020-08-06 10:11:35 -0400

    commit con variable oro, auxiliar y mensajes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :gold="gold"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import store from '@/store';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return store.quantity;
   }
 }
 </script>
@@ -21,8 +24,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <b-container>
+    <b-nav small>
+      <b-nav-item active>Ninja Gold</b-nav-item>
+    </b-nav>
+    <div>
+      <div class="mt-2">Your Gold: {{ gold }}</div>
+    </div>
+    <b-card-group deck>
+      <b-card title="Farm" >
+        <b-card-text>(earns 10-20 golds)</b-card-text>
+        <b-button @click="clickFarm()" variant="primary">Find Gold!</b-button>
+      </b-card>
+      <b-card title="Cave">
+        <b-card-text>(earns 5-10 golds)</b-card-text>
+        <b-button @click="clickCave()" variant="primary">Find Gold!</b-button>
+      </b-card>
+      <b-card title="House">
+        <b-card-text>(earns 2-5 golds)</b-card-text>
+        <b-button @click="clickHouse()" variant="primary">Find Gold!</b-button>
+      </b-card>
+      <b-card title="Casino">
+        <b-card-text>(earns/takes 0-50 golds)</b-card-text>
+        <b-button @click="clickCasino()" variant="primary">Find Gold!</b-button>
+      </b-card>
+    </b-card-group>
+    <label for="textarea">Activities:</label>
+    <b-form-textarea
+      id="textarea"
+      :text="text"
+      placeholder="Enter something..."
+      rows="3"
+      max-rows="6"
+    ></b-form-textarea>
+  </b-container>
 </template>
 
+
 <script>
+import store from '@/store';
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    gold: Number,
+    text: String
+  },
+  methods: {
+    clickFarm() {
+      store.clickFarm();
+    },
+    clickCave() {
+      store.clickCave();
+    },
+    clickHouse() {
+      store.clickHouse();
+    },
+    clickCasino() {
+      store.clickCasino();
+    }
   }
 }
 </script>
@@ -53,6 +76,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #6b2db3;
 }
 </style>
